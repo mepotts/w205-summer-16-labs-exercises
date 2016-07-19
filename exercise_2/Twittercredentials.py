28e7443c5906ac5d85bb66040cf27167ef897b74
mepotts <mepotts@ischool.berkeley.edu>
2016-07-19 15:33:03 -0400

    Update Twittercredentials.py

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "BICrdK3Vj9xVNpr3Po9IssqTU";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "AlwXO6mXxZkuHwUvUlp4sn7OGkGub5gtPZmHMfrVe5E2Qd7Oxu";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "748586503362732033-KSzFNHkwWcx2ejMlOdxTSUednbu9uzZ";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "x88Dt83h3WbhZpiz4eORQNIPLWza4Npi8sH480oizGVpg";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
