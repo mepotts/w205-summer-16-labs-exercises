a9426ac67bef0bf9b05bc6e8b534e86f0f0a7ec2
mepotts <mepotts@ischool.berkeley.edu>
2016-07-28 12:28:06 -0400

    Update psycopg-sample.py

--- exercise_2/psycopg-sample.py
+++ exercise_2/psycopg-sample.py
@@ -7,7 +7,7 @@
 
 import psycopg2
 
-conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+conn = psycopg2.connect(database="tcount", user="w205", password="postgres", host="localhost", port="5432")
 
 
 try:
